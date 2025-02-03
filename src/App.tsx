bb91fc7322eaa765d27c8e9d321f813757f5db8a
cjmiller81 <chris@emergentcybernetics.com>
2025-02-03 17:21:53 -0600

    Fix UI Elements

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,9 @@ import TradingDashboard from './components/TradingDashboard';
 
 function App() {
   return (
-    <>
+    <div className="h-screen w-screen overflow-hidden bg-[#0a0d14]">
       <TradingDashboard />
-    </>
+    </div>
   );
 }
 
